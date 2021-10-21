e4b8f8b7a56e68866211fd324134a1f9ba28a6a0
Jason McCoy <jmccoy@hashicorp.com>
2021-10-21 18:01:02 -0400

    fixed

--- main.tf
+++ main.tf
@@ -6,10 +6,6 @@ module "project_satellite_vpc" {
   source          = "../../modules/satellite_vpc"
 }
 
-#module "project_cloudwatch_dashboard" {
-#  source = "../../modules/cloudwatch_dashboard"
-#}
-
 module "project_ec2" {
   source = "../../modules/ec2"
 }
